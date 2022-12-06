dcfe2a786537412a22441b9b4328b7639f629432
KralMarko123 <kralmarko123@gmail.com>
2022-12-06 22:25:14 +0100

    added metadata

--- reactclient/src/index.jsx
+++ reactclient/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/reset.css";
-import "./styles/general.css";
 import { AuthProvider } from "./custom/useAuth.jsx";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/general.css";
+import "./styles/reset.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
