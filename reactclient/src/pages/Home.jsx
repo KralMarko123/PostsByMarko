b29e4cc5cc0eedd7324fa0589c24ecb124c14aab
KralMarko123 <kralmarko123@gmail.com>
2022-12-30 15:26:04 +0100

    started with filters

--- reactclient/src/pages/Home.jsx
+++ reactclient/src/pages/Home.jsx
@@ -12,23 +12,57 @@ import "../styles/pages/Home.css";
 
 const Home = () => {
 	const appContext = useContext(AppContext);
+	const [filterToggled, setFilterToggled] = useState({
+		isFilterApplied: false,
+		filterType: "",
+	});
+	const [filteredPosts, setFilteredPosts] = useState(appContext.posts);
 	const [isLoading, setIsLoading] = useState(true);
 	const { user } = useAuth();
 	const { lastMessageRegistered } = useSignalR();
 
 	const getPosts = async () => {
 		await PostsService.getAllPosts(user.token)
-			.then((postsFromServer) =>
-				appContext.dispatch({ type: "LOAD_POSTS", posts: postsFromServer })
-			)
+			.then((postsFromServer) => {
+				appContext.dispatch({ type: "LOAD_POSTS", posts: postsFromServer });
+				setFilteredPosts(postsFromServer);
+			})
 			.catch((error) => console.error(error))
 			.then(() => setIsLoading(false));
 	};
 
+	const handleFilterToggle = (e) => {
+		setFilterToggled({
+			isFilterApplied: e.target.checked,
+			filterType: e.target.name,
+		});
+	};
+
 	useEffect(() => {
 		getPosts();
-		console.log("here");
 	}, [lastMessageRegistered]);
+
+	useEffect(() => {
+		if (filterToggled.isFilterApplied) {
+			switch (filterToggled.filterType) {
+				case "myPosts":
+					setFilteredPosts([...filteredPosts.filter((p) => p.userId === user.userId)]);
+					break;
+				case "byLastUpdatedDate":
+					setFilteredPosts([
+						...filteredPosts.sort((p1, p2) => {
+							const date1 = Date.parse(p1.lastUpdatedDate);
+							const date2 = Date.parse(p2.lastUpdatedDate);
+
+							return date1 > date2 ? -1 : 1;
+						}),
+					]);
+
+				default:
+					break;
+			}
+		} else setFilteredPosts(appContext.posts);
+	}, [filterToggled.isFilterApplied]);
 
 	return (
 		<div className="home page">
@@ -39,10 +73,31 @@ const Home = () => {
 				{isLoading ? (
 					<InfoMessage message={"Loading Posts..."} shouldAnimate />
 				) : (
-					<ul className="posts__list">
-						{appContext.posts.length > 0 ? (
-							<>
-								{appContext.posts.map((p) => (
+					<>
+						<div className="posts__dashboard">
+							<div className="dashboard__filters">
+								<p>Filters:</p>
+								<span className="filter">
+									My Posts
+									<input
+										type="checkbox"
+										name="myPosts"
+										id="myPosts"
+										onChange={(e) => handleFilterToggle(e)}
+									/>
+								</span>
+								<span className="filter">
+									By Date
+									<input
+										type="checkbox"
+										name="byLastUpdatedDate"
+										id="byLastUpdatedDate"
+										onChange={(e) => handleFilterToggle(e)}
+									/>
+								</span>
+							</div>
+							<ul className="posts__list">
+								{filteredPosts.map((p) => (
 									<Post
 										key={p.postId}
 										postId={p.postId}
@@ -51,13 +106,11 @@ const Home = () => {
 										content={p.content}
 									/>
 								))}
-								<UpdatePostForm />
-								<DeletePostForm />
-							</>
-						) : (
-							<InfoMessage message={"Seems there are no posts"} />
-						)}
-					</ul>
+							</ul>
+						</div>
+						<UpdatePostForm />
+						<DeletePostForm />
+					</>
 				)}
 			</div>
 		</div>
