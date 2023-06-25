e4bdd64334c43632a9ea689043961363510487d9
KralMarko123 <kralmarko123@gmail.com>
2023-06-25 22:12:16 +0200

    modified development/docker switching

--- PostsByMarko.Host/Program.cs
+++ PostsByMarko.Host/Program.cs
@@ -10,45 +10,30 @@ using System.Text.Json.Serialization;
 using static PostsByMarko.Host.Constants.AppConstants;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var isInDocker = Environment.GetEnvironmentVariable("IS_IN_DOCKER");
+var isInDocker = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 var isInLocalDevelopment = builder.Environment.IsDevelopment();
-
 var mapperConfiguration = new MapperConfiguration(mappperOptions => mappperOptions.AddProfile<UserMappingProfile>());
 var allowedOrigins = builder.Configuration.GetSection("JwtConfig").GetSection("validAudiences").Get<List<string>>();
 var jwtConfig = builder.Configuration.GetSection("JwtConfig");
-
-var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-var dbUserId = Environment.GetEnvironmentVariable("DB_USER_ID");
-var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-var dbPassword = Environment.GetEnvironmentVariable(variable: "MSSQL_SA_PASSWORD");
-
-var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID={dbUserId};Password={dbPassword}";
-if (isInDocker == null) connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
 
 #region ServicesConfiguration
+
 builder.Host.UseSerilog();
 builder.Logging.ClearProviders();
 builder.Logging.SetMinimumLevel(LogLevel.Trace);
-
-builder.WithCors(corsPolicyName, allowedOrigins);
-
+builder.WithCors(corsPolicyName, allowedOrigins!);
 builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
-
 builder.Services.AddSignalR();
 builder.Services.AddDistributedMemoryCache();
-
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.WriteIndented = true;
 });
-
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-
 builder.WithServices();
 builder.WithIdentity();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.WithSwagger();
 builder.WithAuthentication(jwtConfig);
@@ -80,13 +65,11 @@ if (isInLocalDevelopment)
 }
 
 app.UseCors(corsPolicyName);
-if (!isInLocalDevelopment) app.UseHttpsRedirection();
+if (!isInLocalDevelopment || isInDocker != null) app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapHub<PostHub>("/postHub");
 app.MapControllers();
-
 app.UseRateLimiting();
 
 #endregion
