3a75bbbf5984fa7893807ed260bd9abffd2f4efb
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-07-10 10:15:40 +0200

    started with testcontainers for FE tests

--- test/PostsByMarko.FrontendTests/Tests/AuthTests.cs
+++ test/PostsByMarko.FrontendTests/Tests/AuthTests.cs
@@ -1,7 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.Playwright;
 using PostsByMarko.FrontendTests.Tests;
-using PostsByMarko.Host.Data.Models;
 using PostsTesting.UI_Models.Pages;
 using Xunit;
 
@@ -10,21 +9,19 @@ namespace PostsByMarko.FrontendTests.Frontend
     [Collection("Frontend Collection")]
     public class AuthTests
     {
-        private readonly User testUser;
         private readonly IPage page;
         private LoginPage loginPage => new LoginPage(page);
 
         public AuthTests(PostsByMarkoHostFactory postsByMarkoHostFactory)
         {
             page = postsByMarkoHostFactory.page;
-            testUser = postsByMarkoHostFactory.testUser;
         }
 
         [Fact]
         public async Task Test()
         {
             await loginPage.Visit();
-            await loginPage.Login(testUser.UserName, "some_password");
+            await loginPage.Login("test_user@test.com", "some_password");
             await loginPage.errorMessage.WaitForAsync();
 
             var errorTitleText = await loginPage.errorMessage.TextContentAsync();
--- test/PostsByMarko.FrontendTests/Tests/PostsByMarkoHostFactory.cs
+++ test/PostsByMarko.FrontendTests/Tests/PostsByMarkoHostFactory.cs
@@ -1,48 +1,74 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Hosting;
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Images;
 using Microsoft.Playwright;
 using PostsByMarko.FrontendTests.Drivers;
-using PostsByMarko.Host.Data.Models;
-using PostsByMarko.Shared.Constants;
+using PostsByMarko.Test.Shared.Helper;
 using Xunit;
 
 namespace PostsByMarko.FrontendTests.Tests
 {
-    public class PostsByMarkoHostFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+    public class PostsByMarkoHostFactory : IAsyncLifetime
     {
-        public static TestServer testServer { get; private set; }
-        public BrowserDriver driver;
-        public IBrowser browser;
+        private BrowserDriver? driver;
+        private IBrowser? browser;
         public IPage page;
-        public User adminUser = TestingConstants.TEST_USER;
-        public User testUser = TestingConstants.TEST_USER;
+
+        private string? hostDockerfilePath;
+        private IFutureDockerImage? hostImage;
+        private IContainer? hostContainer;
+
 
         public async Task InitializeAsync()
         {
+            ReadDockerfile();
+            SetupHostImageAndContainer();
+            await hostImage!.CreateAsync().ConfigureAwait(false);
+            await hostContainer!.StartAsync().ConfigureAwait(false);
+
             driver = new BrowserDriver();
             browser = await driver.GetChromeBrowserAsync();
             page = await browser.NewPageAsync();
-
-            testServer = Server;
-            var neso = testServer.CreateClient();
-            neso.BaseAddress = new Uri("http://localhost:7171");
         }
 
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        public async Task DisposeAsync()
         {
-            builder.UseEnvironment("Development");
-
-        }
-
-        public async new Task DisposeAsync()
-        {
-            await base.DisposeAsync();
             await page.CloseAsync();
-            await browser.CloseAsync();
+            await browser!.CloseAsync();
             await browser.DisposeAsync();
-            await driver.DestroyPlaywrightAsync();
+            await driver!.DestroyPlaywrightAsync();
+        }
+
+        private void ReadDockerfile()
+        {
+            var slnPath = FileHelper.FindFileDirectory(Directory.GetCurrentDirectory(), "PostsByMarko.sln");
+            var hostFiles = Directory.GetFiles(Path.Combine(slnPath!, $"src\\PostsByMarko.Host"));
+
+            hostDockerfilePath = hostFiles.First(f => f.EndsWith("Dockerfile"));
+        }
+
+        private void SetupHostImageAndContainer()
+        {
+            try
+            {
+                hostImage = new ImageFromDockerfileBuilder()
+                .WithName("postsbymarko.host")
+                .WithDockerfile(hostDockerfilePath)
+                .WithDeleteIfExists(false)
+                .Build();
+
+                hostContainer = new ContainerBuilder()
+                .WithImage(hostImage.FullName)
+                .WithPortBinding(7171, 7171)
+                .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(7171))
+                .Build();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
     }
 }
