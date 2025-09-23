c99e48cb5203f633ceca0bfd89016f5c2d4ad367
KralMarko123 <kralmarko123@gmail.com>
2025-09-23 17:28:40 +0200

    fix: missing jwt configuration for signalR

--- src/PostsByMarko.Client/src/custom/useSignalR.jsx
+++ src/PostsByMarko.Client/src/custom/useSignalR.jsx
@@ -1,49 +1,59 @@
 import { React, useState, useEffect } from "react";
-import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import {
+  HttpTransportType,
+  HubConnectionBuilder,
+  LogLevel,
+} from "@microsoft/signalr";
 import ENDPOINT__URLS from "../constants/endpoints";
 import { useAuth } from "./useAuth";
 
 export const useSignalR = () => {
-	const { user } = useAuth();
-	const [signalR, setSignalR] = useState({
-		connection: new HubConnectionBuilder()
-			.withUrl(ENDPOINT__URLS.HUB, {
-				accessTokenFactory: () => user.token,
-			})
-			.configureLogging(LogLevel.Information)
-			.withAutomaticReconnect()
-			.build(),
+  const { user } = useAuth();
+  const [signalR, setSignalR] = useState({
+    connection: new HubConnectionBuilder()
+      .withUrl(ENDPOINT__URLS.HUB, {
+        accessTokenFactory: () => user.token,
+      })
+      .configureLogging(LogLevel.Information)
+      .withAutomaticReconnect()
+      .build(),
 
-		async sendMessage(message, toAll = true) {
-			await sendMessageToHub(message, toAll);
-		},
+    async sendMessage(message, toAll = true) {
+      await sendMessageToHub(message, toAll);
+    },
 
-		lastMessageRegistered: "",
-	});
+    lastMessageRegistered: "",
+  });
 
-	useEffect(() => {
-		if (signalR.connection) {
-			signalR.connection
-				.start()
-				.then(() => {
-					signalR.connection.on("ReceiveMessage", (message) => {
-						console.log(message);
-						setSignalR({ ...signalR, lastMessageRegistered: message });
-					});
-				})
-				.catch((error) => console.error(`SignalR connection	 failed with error: ${error}`));
-		}
-	}, [signalR.connection]);
+  useEffect(() => {
+    if (signalR.connection) {
+      signalR.connection
+        .start()
+        .then(() => {
+          signalR.connection.on("ReceiveMessage", (message) => {
+            console.log(message);
+            setSignalR({ ...signalR, lastMessageRegistered: message });
+          });
+        })
+        .catch((error) =>
+          console.error(`SignalR connection	failed with error: ${error}`)
+        );
+    }
+  }, [signalR.connection]);
 
-	const sendMessageToHub = async (message, toAll = false) => {
-		if (signalR.connection) {
-			try {
-				await signalR.connection.invoke(toAll ? "SendMessageToAll" : "SendMessageToOthers", message);
-			} catch (error) {
-				console.error(error);
-			}
-		} else console.log("A connection to the server hasn't been established yet!");
-	};
+  const sendMessageToHub = async (message, toAll = false) => {
+    if (signalR.connection) {
+      try {
+        await signalR.connection.invoke(
+          toAll ? "SendMessageToAll" : "SendMessageToOthers",
+          message
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    } else
+      console.log("A connection to the server hasn't been established yet!");
+  };
 
-	return signalR;
+  return signalR;
 };
--- src/PostsByMarko.Host/Extensions/WebApplicationBuilderExtensions.cs
+++ src/PostsByMarko.Host/Extensions/WebApplicationBuilderExtensions.cs
@@ -103,6 +103,24 @@ namespace PostsByMarko.Host.Extensions
                     ValidAudiences = jwtConfig.GetSection("validAudiences").Get<List<string>>(),
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig["secret"])),
                 };
+                options.Authority = jwtConfig["authority"];
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        // If the request is for our hub...
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/postHub")))
+                        {
+                            // Read the token out of the query string
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             });
         }
 
