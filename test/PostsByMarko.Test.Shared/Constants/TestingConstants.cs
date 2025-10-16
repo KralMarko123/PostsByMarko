994755073968d4a68983f892c4dfa7b934a20079
KralMarko123 <kralmarko123@gmail.com>
2025-10-16 17:32:09 +0200

    fix

--- test/PostsByMarko.Test.Shared/Constants/TestingConstants.cs
+++ test/PostsByMarko.Test.Shared/Constants/TestingConstants.cs
@@ -13,7 +13,7 @@ namespace PostsByMarko.Test.Shared.Constants
         public const string DEV_CLIENT_ENDPOINT = "http://localhost:3000";
         public const string FIREFOX_BROWSER = "Firefox";
         public const string CHROME_BROWSER = "Chrome";
-        public const bool HEADLESS_BROWSER = false;
+        public const bool HEADLESS_BROWSER = true;
         public const int UI_TIMEOUT_IN_MILLISECONDS = 10000;
     }
 }
